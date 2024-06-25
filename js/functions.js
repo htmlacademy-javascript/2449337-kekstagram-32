bd8355729f009e620ce632aa06fa778a59f95712
KolesnikovaAlena <108642alena@gmail.com>
2024-06-25 19:53:23 +0300

    Исправление ошибок

--- js/functions.js
+++ js/functions.js
@@ -1,12 +1,12 @@
 // Функия которая измеряет длину строки
-const measurementLeight = (string, maxLenght) => {
-  string <= maxLenght;
-};
+const measurementLenght = (string, maxLenght) => string <= maxLenght;
+
 
 // Функция которая проверяет строку на палиндром (Слово или фраза которые читаются вперёд и назад одинаково. Например: потом или шалаш)
 
 const checkPalindrom = (string) => {
-  let normString = string.replaceAll(' ', ''), string.toLowerCase();
-  let reverseString = string.replaceAll(' ', ''), string.toLowerCase(), string.reverse()
+  const normString = string.replaceAll(' ', '').toLowerCase();
+  const reverseString = normString.split('').reverse().join('');
   return normString === reverseString;
 };
+
