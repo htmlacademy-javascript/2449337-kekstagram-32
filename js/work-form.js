f98a0732b0f5bbe55ba9130e1042a6b02b9add8d
KolesnikovaAlena <157299773+KolesnikovaAlena@users.noreply.github.com>
2024-07-30 19:34:07 +0400

    Правда или действие (#7)
    * Merge pull request #1 from KolesnikovaAlena/master
    
    Добавляет наставника
    
    * Незначительное изменение
    
    * Merge pull request #2 from KolesnikovaAlena/module1-task1
    
    * Добавление 2 функций
    
    * Исправление ошибок
    
    * Merge pull request #3 from KolesnikovaAlena/module2-task1
    
    * Подключение main.js
    
    * Добавление id и url
    
    * Добавление фото
    
    * Попытка исправить ошибки
    
    * Исправление ошибок 2
    
    * Небольшие изменения
    
    * Окончательное исправление
    
    * Merge branch 'master' into module4-task1
    
    * Merge pull request #4 from KolesnikovaAlena/module4-task1
    
    * Merge branch 'master' into module5-task1
    
    * Merge pull request #5 from KolesnikovaAlena/module5-task1
    
    * Функции возвращаются
    
    * Merge pull request #6 from KolesnikovaAlena/module5-task2
    
    * Попытка выполнить задание "Отрисуй меня полностью"
    
    * Исправление ошибок
    
    * Merge branch 'master' into module7-task1
    
    * Merge pull request #7 from KolesnikovaAlena/module7-task1
    
    * Добавлние функции и обработчиков событий
    
    * Улучшение обработиков и функций
    
    * Добавлнеие import и export
    
    * Попытка исправить ошибки
    
    * Небольшие правки
    
    * Значительные правки часть 1
    
    * Значительные правки часть 2
    
    * Merge branch 'master' into module8-task1
    
    * Merge pull request #8 from KolesnikovaAlena/module8-task1
    
    * Выполнение задания "Правда или действие" и настройка закрытия карточек
    
    * Окончательное исправление ошибок!!!
    
    * Merge branch 'master' into module9-task1

---
+++ js/work-form.js
@@ -0,0 +1,105 @@
+// Переменные для настройки хештегов
+const maxHashtagCount = 5;
+const valudSimvols = /^#[a-zа-яё0-9]{1,19}$/i;
+const errorText = {
+  invalidCount: `Максисум ${maxHashtagCount} xeштегов`,
+  notUnique: 'Хештеги должны быть уникальными',
+  invalidPattern: 'Неправильный хештег'
+};
+
+// Остальные переменные
+
+const body = document.querySelector('body');
+const form = document.querySelector('.img-upload__form');
+const overlay = form.querySelector('.img-upload__overlay');
+const closeButton = form.querySelector('.img-upload__cancel');
+const fileField = form.querySelector('.img-upload__input');
+const hastagField = form.querySelector('.text__hashtags');
+const commentField = form.querySelector('.text__description');
+
+const pristine = new Pristine(form, {
+  classTo: 'img-upload__field-wrapper',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextClass: 'img-upload__field-wrapper__error'
+});
+
+const showModal = () => {
+  overlay.classList.remove('hidden');
+  body.classList.add('modal-open');
+  document.addEventListener('keydown', onDocumentKeydown);
+};
+
+const hideModal = () => {
+  form.reset();
+  pristine.reset();
+  overlay.classList.add('hidden');
+  body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onDocumentKeydown);
+};
+
+const isTextFieldFocused = () =>
+  document.activeElement === hastagField ||
+  document.activeElement === commentField;
+
+const normalizeTags = (tagString) => tagString
+  .trim()
+  .split(' ')
+  .filter((tag) => Boolean(tag.length));
+
+const hasValidTags = (value) => normalizeTags(value).every((tag) => valudSimvols.test(tag));
+const hasValidCount = (value) => normalizeTags(value).length <= maxHashtagCount;
+
+const hasUniqueTags = (value) => {
+  const lowerCaseTags = normalizeTags(value).map((tag) => tag.toLowerCase());
+  return lowerCaseTags.length === new Set(lowerCaseTags).size;
+};
+
+function onDocumentKeydown(evt) {
+  if (evt.key === 'Escape' && !isTextFieldFocused()) {
+    evt.preventDefault();
+    hideModal();
+  }
+}
+
+const onCloseButtonClick = () => {
+  hideModal();
+};
+
+const onFileInputChange = () => {
+  showModal();
+};
+
+// Функция которая отменяет действие по умолчанию (отправление) и проводит валидацию
+const onFormSubmit = (evt) => {
+  evt.preventDefault();
+  pristine.validate();
+};
+
+// Проверки правильности хештегов
+pristine.addValidator(
+  hastagField,
+  hasValidCount,
+  errorText.invalidCount,
+  3,
+  true
+);
+
+pristine.addValidator(
+  hastagField,
+  hasUniqueTags,
+  errorText.notUnique,
+  2,
+  true
+);
+
+pristine.addValidator(
+  hastagField,
+  hasValidTags,
+  errorText.invalidPattern,
+  1,
+  true
+);
+
+fileField.addEventListener('change', onFileInputChange);
+closeButton.addEventListener('click', onCloseButtonClick);
+form.addEventListener('submit', onFormSubmit);
