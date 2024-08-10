fd7427c6ec09f9ae3e186b5001ece270b32dd80c
KolesnikovaAlena <108642alena@gmail.com>
2024-08-10 18:20:36 +0300

    Выполнение задания Фото на память

--- js/work-form.js
+++ js/work-form.js
@@ -7,6 +7,7 @@ import {
 // Переменные для настройки хештегов
 const maxHashtagCount = 5;
 const valudSimvols = /^#[a-zа-яё0-9]{1,19}$/i;
+const FILE_TYPES = ['jpg', 'jpeg', 'png'];
 const errorText = {
   invalidCount: `Максисум ${maxHashtagCount} xeштегов`,
   notUnique: 'Хештеги должны быть уникальными',
@@ -28,6 +29,8 @@ const fileField = form.querySelector('.img-upload__input');
 const hastagField = form.querySelector('.text__hashtags');
 const commentField = form.querySelector('.text__description');
 const submitButton = form.querySelector('.img-upload__submit');
+const photoPreview = form.querySelector('.img-upload__preview img');
+const effectsPreviews = form.querySelectorAll('.effects__preview');
 
 const pristine = new Pristine(form, {
   classTo: 'img-upload__field-wrapper',
@@ -62,6 +65,12 @@ const isTextFieldFocused = () =>
   document.activeElement === hastagField ||
   document.activeElement === commentField;
 
+const isValidType = (file) => {
+  const fileName = file.name.toLowerCase();
+  // Объясните, что возвращает, пожалуйста
+  return FILE_TYPES.some((it) => fileName.endsWith(it));
+};
+
 const normalizeTags = (tagString) => tagString
   .trim()
   .split(' ')
@@ -70,6 +79,7 @@ const normalizeTags = (tagString) => tagString
 const hasValidTags = (value) => normalizeTags(value).every((tag) => valudSimvols.test(tag));
 const hasValidCount = (value) => normalizeTags(value).length <= maxHashtagCount;
 
+// Что делает Boolean?
 const isErrorMessageShown = () => Boolean(document.querySelector('.error'));
 
 const hasUniqueTags = (value) => {
@@ -89,6 +99,14 @@ const onCloseButtonClick = () => {
 };
 
 const onFileInputChange = () => {
+  const file = fileField.files[0];
+
+  if (file && isValidType(file)) {
+    photoPreview.src = URL.createObjectURL(file);
+    effectsPreviews.forEach((preview) => {
+      preview.style.backgroundImage = `url('${photoPreview.src}')`;
+    });
+  }
   showModal();
 };
 
